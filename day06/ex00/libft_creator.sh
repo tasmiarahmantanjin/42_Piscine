cf08853a68a58de701b72ace8f6d5265d1c96ab2
Tasmia Rahman <60643060+tasmiarahmantanjin@users.noreply.github.com>
2020-05-29 19:30:20 +0300

    Add files via upload

---
+++ day06/ex00/libft_creator.sh
@@ -0,0 +1,2 @@
+gcc -c *.c
+ar cr libft.a *.o
---
+++ day06/ex02/ft_print_params.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/12 17:13:22 by trahman           #+#    #+#             */
+/*   Updated: 2020/02/12 20:22:26 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+int		main(int argc, char **argv)
+{
+	int i;
+	int arg;
+
+	arg = 1;
+	while (arg < argc)
+	{
+		i = 0;
+		while (argv[arg][i] != '\0')
+		{
+			ft_putchar(argv[arg][i]);
+			i++;
+		}
+		i = 0;
+		ft_putchar('\n');
+		arg++;
+	}
+	return (0);
+}
